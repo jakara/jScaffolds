522108fab2dbd7fe2ca2a8cd2fa5c268d925ba9d
付杰 <jiefu@tujia.com>
2017-05-11 17:05:26 +0800

    init

---
+++ jscaffolds-console/src/main/java/com/jakara/jscaffolds/console/MainApp.java
@@ -0,0 +1,10 @@
+package com.jakara.jscaffolds.console;
+
+/**
+ * @author jiefu 2017/5/11 17:32
+ */
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
---
+++ jscaffolds-domain/src/main/java/com/jakara/jscaffolds/domain/seedwork/Entity.java
@@ -0,0 +1,7 @@
+package com.jakara.jscaffolds.domain.seedwork;
+
+/**
+ * @author jiefu 2017/5/11 17:43
+ */
+public class Entity {
+}
